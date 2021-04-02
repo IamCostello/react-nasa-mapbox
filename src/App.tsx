57d60c22ca28c8fb2c078e7d5012d856038b0385
IamCostello <mynameiscostello@gmail.com>
2021-04-02 19:07:32 +0200

    Load project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Stack } from "@chakra-ui/layout";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack direction={{ base: "column", lg: "row" }}>
+      <Box>Nasa view</Box>
+      <Box>Map view</Box>
+    </Stack>
   );
 }
 
