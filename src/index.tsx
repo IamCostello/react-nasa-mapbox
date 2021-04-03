59235cf03420ae77efb7f713e2f3668871b154af
IamCostello <mynameiscostello@gmail.com>
2021-04-03 09:09:55 +0200

    Fix worker loader

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 import App from "./pages/App";
 import { ViewportProvider } from "./providers/viewport";
 import nightMode from "./theme/theme";
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 ReactDOM.render(
   <React.StrictMode>
